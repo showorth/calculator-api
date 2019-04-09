5c70ecbcd1a7ca0e45c3972dcfcf73ff27179b83
Steph <steph.porretta.howorth@gmail.com>
2019-04-09 19:55:27 -0400

    added add and subtract methods

---
+++ express.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+
+
+
+// allow CORS from dev client
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+})
+
+app.get('/add', function (req, res) {
+    var firstNumber = req.query.firstNumber || 0;
+    var secondNumber = req.query.secondNumber || 0;
+    var result = Number(firstNumber) + Number(secondNumber);
+    res.send({result})
+
+})
+
+app.get('/subtract', function (req, res) {
+    var firstNumber = req.query.firstNumber || 0;
+    var secondNumber = req.query.secondNumber || 0;
+    var result = Number(firstNumber) - Number(secondNumber);
+    res.send({result})
+
+})
+
+module.exports = app;
